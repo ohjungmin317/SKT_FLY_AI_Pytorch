ed0c25c6d25a0d6aacf5370959553aa86da288cb
ohjungmin <jmin980317@naver.com>
2023-07-24 15:51:55 +0900

    Study for Pytorch

---
+++ chap5/5-5-2.py
@@ -0,0 +1,133 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+import torch
+import torch.nn as nn
+from torch.autograd import Variable
+import torch.nn.functional as F
+
+import torchvision
+import torchvision.transforms as transforms
+from torch.utils.data import Dataset, DataLoader
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+mnist_transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5,), (1.0,))])
+
+from torchvision.datasets import MNIST
+import requests
+
+download_root = './data/MNIST_DATASET'
+
+train_dataset = torchvision.datasets.FashionMNIST(root=download_root, download=False,
+                                           transform=transforms.Compose([transforms.ToTensor()]))
+test_dataset = torchvision.datasets.FashionMNIST(root=download_root, download=False,
+                                          train=False,
+                                          transform=transforms.Compose([transforms.ToTensor()]))
+
+labels_map = {0 : 'T-shirt', 1 : 'Trouser', 2: 'Pullover', 3: 'Dress', 4: 'Coat', 5: 'Sandal', 6: 
+              'Shirt', 7: 'Sneaker', 8:'Bag', 9:'Ankle Boot'}
+
+train_loader = torch.utils.data.DataLoader(train_dataset,batch_size=100)
+
+test_loader = torch.utils.data.DataLoader(test_dataset,batch_size=100)
+
+fig = plt.figure(figsize=(8,8))
+columns = 4;
+rows=5;
+for i in range(1, columns * rows +1):
+    img_xy = np.random.randint(len(train_dataset));
+    img = train_dataset[img_xy][0][0,:,:]
+    fig.add_subplot(rows, columns, i)
+    plt.title(labels_map[train_dataset[img_xy][1]])
+    plt.axis('off')
+    plt.imshow(img, cmap='gray')
+plt.show()
+
+# 합성곱 신경망
+class FashionCNN(nn.Module):
+
+    def __init__(self):
+        super(FashionCNN, self).__init__()
+        self.layer1 = nn.Sequential(
+            nn.Conv2d(in_channels=1, out_channels=32, kernel_size=3, padding=1),
+            nn.BatchNorm2d(num_features=32),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2))
+
+        self.layer2 = nn.Sequential(
+            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3),
+            nn.BatchNorm2d(num_features=64),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2))
+
+        self.fc1 = nn.Linear(in_features=64 * 6 * 6, out_features=600)
+        self.drop = nn.Dropout2d(0.25)
+        self.fc2 = nn.Linear(in_features=600, out_features=120)
+        self.fc3 = nn.Linear(in_features=120, out_features=10) # 마지막 계층의 out_features는 클래스 개수를 의미
+
+    def forward(self, x):
+        out = self.layer1(x)
+        out = self.layer2(out)
+        out = out.view(out.size(0), -1) # Flatten
+        out = self.fc1(out)
+        out = self.drop(out)
+        out = self.fc2(out)
+        out = self.fc3(out)
+        return out
+    
+learning_rate = 0.001;
+model = FashionCNN();
+model.to(device)
+
+criterion = nn.CrossEntropyLoss();
+optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate);
+print(model)
+
+# 합성곱 신경망을 이용한 학습 모델
+
+num_epochs = 5
+count = 0
+
+loss_list = []
+iteration_list = []
+accuracy_list = []
+
+predictions_list = []
+labels_list = []
+
+for epoch in range(num_epochs):
+    for images, labels in train_loader:
+        images, labels = images.to(device), labels.to(device)
+
+        train = Variable(images.view(100, 1, 28, 28))
+        labels = Variable(labels)
+
+        outputs = model(train)
+        loss = criterion(outputs, labels)
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        count += 1
+
+        if not (count % 50):
+            total = 0
+            correct = 0
+            for images, labels in test_loader:
+                images, labels = images.to(device), labels.to(device)
+                labels_list.append(labels)
+                test = Variable(images.view(100, 1, 28, 28))
+                outputs = model(test)
+                predictions = torch.max(outputs, 1)[1].to(device)
+                predictions_list.append(predictions)
+                correct += (predictions == labels).sum()
+                total += len(labels)
+
+            accuracy = correct * 100 / total
+            loss_list.append(loss.data)
+            iteration_list.append(count)
+            accuracy_list.append(accuracy)
+
+        if not (count % 500):
+            print("Iteration: {}, Loss: {}, Accuracy: {}%".format(count, loss.data, accuracy))
+
